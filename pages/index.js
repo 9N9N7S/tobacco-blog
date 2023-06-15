811ab3afe1bb6712dab3f203391aa410068c17d9
9N9N7S <wang.yi.qi@mail.ru>
2023-06-16 04:26:02 +0800

    re

--- pages/index.js
+++ pages/index.js
@@ -51,10 +51,10 @@ export default function Index({ posts, globalData }) {
         variant="large"
         className="fixed top-20 opacity-40 dark:opacity-60"
       />
-      {/* <GradientBackground
+      <GradientBackground
         variant="small"
         className="absolute bottom-0 opacity-20 dark:opacity-10"
-      /> */}
+      />
     </Layout>
   );
 }
